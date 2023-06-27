4015ce02324bc6c845fa976ded42d6cccfb27e23
nthnn <nathanneisip@gmail.com>
2023-06-27 18:20:30 +0800

    Fixed definition namings.

--- src/ph4502c_sensor.h
+++ src/ph4502c_sensor.h
@@ -3,9 +3,9 @@
 
 #include <Arduino.h>
 
-#define PH4502_DEFAULT_CALIBRATION 14.8f
-#define PH4502_DEFAULT_READING_INTERVAL 100
-#define PH4502_DEFAULT_READING_COUNT 10
+#define PH4502C_DEFAULT_CALIBRATION 14.8f
+#define PH4502C_DEFAULT_READING_INTERVAL 100
+#define PH4502C_DEFAULT_READING_COUNT 10
 
 class PH4502C_Sensor {
     public:
@@ -13,23 +13,23 @@ class PH4502C_Sensor {
     PH4502C_Sensor(uint16_t ph_level_pin, uint16_t temp_pin):
         _ph_level_pin(ph_level_pin),
         _temp_pin(temp_pin),
-        _calibration(PH4502_DEFAULT_CALIBRATION),
-        _reading_interval(PH4502_DEFAULT_READING_INTERVAL),
-        _reading_count(PH4502_DEFAULT_READING_COUNT) { }
+        _calibration(PH4502C_DEFAULT_CALIBRATION),
+        _reading_interval(PH4502C_DEFAULT_READING_INTERVAL),
+        _reading_count(PH4502C_DEFAULT_READING_COUNT) { }
 
     PH4502C_Sensor(uint16_t ph_level_pin, uint16_t temp_pin, float calibration):
         _ph_level_pin(ph_level_pin),
         _temp_pin(temp_pin),
         _calibration(calibration),
-        _reading_interval(PH4502_DEFAULT_READING_INTERVAL),
-        _reading_count(PH4502_DEFAULT_READING_COUNT) { }
+        _reading_interval(PH4502C_DEFAULT_READING_INTERVAL),
+        _reading_count(PH4502C_DEFAULT_READING_COUNT) { }
 
     PH4502C_Sensor(uint16_t ph_level_pin, uint16_t temp_pin, float calibration, int reading_interval):
         _ph_level_pin(ph_level_pin),
         _temp_pin(temp_pin),
         _calibration(calibration),
         _reading_interval(reading_interval),
-        _reading_count(PH4502_DEFAULT_READING_COUNT) { }
+        _reading_count(PH4502C_DEFAULT_READING_COUNT) { }
 
     PH4502C_Sensor(uint16_t ph_level_pin, uint16_t temp_pin, float calibration, int reading_interval, int reading_count):
         _ph_level_pin(ph_level_pin),
